126831f6eb703b2f53e0757dcb567c3ff7f6531a
Ashish D <adhawale@covisum.com>
2020-01-08 13:31:04 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Hey Welcome to React Ashish Awesome</code> 
         </p>
         <a
           className="App-link"
